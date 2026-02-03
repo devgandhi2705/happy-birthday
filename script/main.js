ade0c137ce6850bb662443b4075ce7299cba86c6
Dev gandhi <66508336+devgandhi2705@users.noreply.github.com>
2026-02-03 15:54:39 +0530

    Update prompt text from 'Coba pencet deh' to 'Try Pressing Me!!'

--- script/main.js
+++ script/main.js
@@ -350,7 +350,7 @@ let x = setInterval(function () {
                 <div class="flame"></div>
             </div>
             <div class="prompt">
-            Coba pencet deh
+            Try Pressing Me!!
             </div>
         </div>
     `;
